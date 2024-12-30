665b079ec248e7d21b9b9b65d5033d8d7e2a8ba2
Abdenour TEGGOUR <teggourabdenour@gmail.com>
2024-12-30 18:55:22 +0100

    chore : create gaming pass api project

---
+++ gamig-pass-api/src/main/java/com/madaffrager/pass/GamigPassApiApplication.java
@@ -0,0 +1,13 @@
+package com.madaffrager.pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamigPassApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamigPassApiApplication.class, args);
+	}
+
+}
---
+++ gamig-pass-api/src/test/java/com/madaffrager/pass/GamigPassApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madaffrager.pass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamigPassApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
